a05cdc10cc16046d06e7d2e50d686d560e5a9c47
Fabio916 <ferreirafaabio04@gmail.com>
2023-11-24 09:52:45 -0300

    adicionado o sql e o docx

--- database/migrations/2023_11_23_231447_fornecedores.php
+++ database/migrations/2023_11_23_231447_fornecedores.php
@@ -16,8 +16,6 @@ return new class extends Migration
             $table->string('nome');
             $table->string('uf');
             $table->string('email');
-            $table->dropColumn('site');
-
     });
 
 }
